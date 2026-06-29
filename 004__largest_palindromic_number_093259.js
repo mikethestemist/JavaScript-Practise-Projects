ddb88feac549edbf387637ac81fd364710474a52
Ireoluwa Michael Sholanke <mikethestemist@gmail.com>
2026-06-29 17:59:54 +0100

    Enhance palindrome query function for 2 and 3 digits
    Refactor queryNumbers function to support 3-digit numbers and adjust logic for independent digits.

--- 004__largest_palindromic_number_093259.js
+++ 004__largest_palindromic_number_093259.js
@@ -12,30 +12,53 @@ function isPalindrome(num) {
 // console.log(isPalindrome(554));
 
 let palindromes = {};
-function queryNumbers(noOfTwoDigits) { // enter either 2 or 3
-    for (let i = 0; i < 100; i++) {
-        for (let j = 0; j < 100; j++) {
+function queryNumbers(noOfIndependentDigits, countOfDigitLength = 2) { // enter either 2 or 3
+    if (countOfDigitLength == 2) {
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
 
-            if (noOfTwoDigits == 2) {
-                let prod = i * j;
-                if (isPalindrome(prod)) {
-                    palindromes[prod] = i + ', ' + j;
-                }
-
-            } else if (noOfTwoDigits == 3) {
-                for (let k = 0; k < 100; k++) {
-                    let prod = i * j * k;
+                if (noOfIndependentDigits == 2) {
+                    let prod = i * j;
                     if (isPalindrome(prod)) {
-                        palindromes[prod] = i + ', ' + j + ', ' + k;
+                        palindromes[prod] = i + ', ' + j;
+                    }
 
+                } else if (noOfIndependentDigits == 3) {
+                    for (let k = 0; k < 100; k++) {
+                        let prod = i * j * k;
+                        if (isPalindrome(prod)) {
+                            palindromes[prod] = i + ', ' + j + ', ' + k;
+
+                        }
                     }
                 }
             }
+        }
+    } else if (countOfDigitLength == 3) {
+        for (let i = 100; i < 1000; i++) {
+            for (let j = 100; j < 1000; j++) {
 
+                if (noOfIndependentDigits == 2) {
+                    let prod = i * j;
+                    if (isPalindrome(prod)) {
+                        palindromes[prod] = i + ', ' + j;
+                    }
+
+                } else if (noOfIndependentDigits == 3) {
+                    for (let k = 100; k < 1000; k++) {
+                        let prod = i * j * k;
+                        if (isPalindrome(prod)) {
+                            palindromes[prod] = i + ', ' + j + ', ' + k;
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
-queryNumbers(2);
+// queryNumbers(2);
+queryNumbers(2, 3);
 console.log(palindromes);
 
 
